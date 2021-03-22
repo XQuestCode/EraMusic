456e21346bcd1a6bdbaaa3b30f1f2c1ebd0c88fd
Nirmal Patel <73463298+NIRMAL1090@users.noreply.github.com>
2021-03-22 17:41:52 +0530

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -3,7 +3,7 @@ module.exports = {
     aliases: ["blast"],
 
     execute: async (client, message, args) => {
-        if (message.author.id !== '512520979788857355') {
+        if (message.author.id !== '679561036646449178') {
             return message.channel.send(`Only <@512520979788857355> Can Use This Command.`)
         }
         await message.channel.send(`Restarting bot :)`)
