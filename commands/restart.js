8026da9c187c58a4d334df48640a38c0f765ec10
Nirmal Patel <73463298+NIRMAL1090@users.noreply.github.com>
2021-03-24 14:55:21 +0530

    DELETED...

--- commands/restart.js
+++
@@ -1,8 +0,0 @@
-
-
-if (command === "restart") {
-    if (message.author.id !== "679561036646449178") return false;
-    message.reply("Restarting...");
-    process.destroy();
-    require("child_process").exec("node index.js");
-};
